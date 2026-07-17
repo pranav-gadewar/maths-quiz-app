9b01045aca3d7227f3cc63aae6fdbeb89d03a199
pranav-gadewar <gadewar.pranav03@gmail.com>
2026-07-18 01:44:48 +0530

    enhanced the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Dashboard from "../components/Dashboard";
 
+
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
       <Navbar />
       <Dashboard />
+
       <Footer />
     </main>
   );
